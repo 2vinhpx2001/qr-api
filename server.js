a0901a01de977408ce12bc54feac62294f40bced
Vinh Pham <2vinhpx2001@gmail.com>
2025-05-26 11:58:35 +0700

    update

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
+
 const { connectMongo } = require('./config/dbMongo');
 const qrRoutes = require('./routes/qrRoutes');
 
 dotenv.config();
 const app = express();
+
+// Load SSL cert & key
+const sslOptions = {
+  key: fs.readFileSync(path.resolve(__dirname, 'ssl/cert.key')),
+  cert: fs.readFileSync(path.resolve(__dirname, 'ssl/cert.crt'))
+};
 
 app.use(cors());
 app.use(express.json());
@@ -14,5 +24,7 @@ app.use('/api/qr', qrRoutes);
 (async () => {
   await connectMongo();
   const PORT = process.env.PORT || 8080;
-  app.listen(PORT, () => console.log(`✅ QR API chạy tại cổng ${PORT}`));
+  https.createServer(sslOptions, app).listen(PORT,'0.0.0.0', () => {
+    console.log(`✅ HTTPS QR API chạy tại https://localhost:${PORT}`);
+  });
 })();
